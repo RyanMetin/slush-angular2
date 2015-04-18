072777da188c2aba3abada9cf1e6d667b797800a
Ryan Metin <rmusumi@hotmail.com>
2015-04-18 04:03:22 -0400

    switching out quickstart for gulp build

--- templates/gulpfile.js
+++ templates/gulpfile.js
@@ -1,6 +1,44 @@
 var gulp = require('gulp'),
   connect = require('gulp-connect'),
-  open = require('opn');
+  del = require('del'),
+  open = require('opn'),
+  plumber = require('gulp-plumber'),
+  traceur = require('gulp-traceur');
+
+gulp.task('html', function() {
+  gulp.src('./src/*.html')
+    .pipe(gulp.dest('build'))
+    .pipe(connect.reload());
+});
+
+gulp.task('js', function() {
+  gulp.src('./src/app/*.js')
+    .pipe(rename({extname:''}))
+    .pipe(plumber())
+    .pipe(traceur({
+      annotations: true,
+      memberVariables: true,
+      moduleName: true,
+      modules: 'instantiate',
+      types: true
+    }))
+    .pipe(rename({extname:'.js'}))
+    .pipe(gulp.dest('build'))
+    .pipe(connect.reload());
+});
+
+gulp.task('lib', function() {
+  gulp.src([
+    'node_modules/angular2/node_modules/traceur/bin/traceur.js',
+    'node_modules/angular2/node_modules/traceur/bin/traceur-runtime.js',
+    'node_modules/angular2/node_modules/zone.js/long-stack-trace-zone.js',
+    'node_modules/angular2/node_modules/zone.js/zone.js',
+    'node_modules/es6-module-loader/dist/es6-module-loader.js',
+    'node_modules/es6-module-loader/dist/es6-module-loader.js.map',
+    'node_modules/systemjs/dist/system.js',
+    'node_modules/systemjs/dist/system.js.map'
+  ])
+});
 
 gulp.task('serve', function() {
   connect.server({
@@ -9,16 +47,6 @@ gulp.task('serve', function() {
     livereload: true
   });
   open('http://localhost:5050');
-});
-
-gulp.task('html', function() {
-  gulp.src('./src/*.html')
-    .pipe(connect.reload());
-});
-
-gulp.task('js', function() {
-  gulp.src('./src/app/*.js')
-    .pipe(connect.reload());
 });
 
 gulp.task('watch', function() {
