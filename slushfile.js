1d5795e66d527feb6a3bb69c25ab3f6a49411c39
Ryan Metin <rmusumi@hotmail.com>
2015-07-22 03:21:50 -0400

    ...

--- slushfile.js
+++ slushfile.js
@@ -1,18 +1,24 @@
 var gulp = require('gulp'),
     conflict = require('gulp-conflict'),
+    fs = require('fs'),
+    ini = require('ini'),
     install = require('gulp-install'),
     inquirer = require('inquirer'),
     path = require('path'),
+    rename = require('gulp-rename'),
     template = require('gulp-template');
+
+var config = ini.parse(fs.readFileSync(process.env.HOME + '/.gitconfig', 'utf-8'));
 
 gulp.task('default', function (cb) {
   inquirer.prompt([
     {
       type: 'input',
       message: 'Name your project:',
-      name: 'name', 
-      validate: function (answer) {
-        if (!answer) {
+      name: 'name',
+      default: 'slushy',
+      validate: function (input) {
+        if (!input) {
           return 'Seriously, name your project:';
         } else {
           return true;
@@ -20,14 +26,14 @@ gulp.task('default', function (cb) {
       }
     }, {
       type: 'list',
-      message: 'Choose your scaffold:',
+      message: 'Choose a scaffold:',
       name: 'scaffold',
       choices: ['basic', 'boiler'],
       default: 'boiler'
     }, {
       type: 'list',
-      message: 'Choose your script:',
-      name: 'syntax',
+      message: 'Choose an ECMAScript standard:',
+      name: 'spec',
       choices: ['es5', 'es6'],
       default: 'es6'
     }, {
@@ -35,9 +41,16 @@ gulp.task('default', function (cb) {
       message: 'Use TypeScript?',
       name: 'ts',
       default: true
+    }, {
+      type: 'confirm',
+      message: 'Everything look good?',
+      name: 'good'
     }
   ],
   function (answers) {
+    if (!answers.good) {
+      return cb();
+    }
     answers.slug = slugify(answers.name);
     answers.camel = camelize(answers.name);
     path.resolve(process.cwd(), answers.slug);
